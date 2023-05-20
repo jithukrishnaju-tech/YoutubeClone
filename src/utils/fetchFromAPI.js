406a9ae8679e9a033bfa1135a80c561eebfe3bc0
Jithu <jithukrishnaju@gmail.com>
2023-05-20 18:34:01 +0530

    youtube clone commit

---
+++ src/utils/fetchFromAPI.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+const BASE_URL = "https://youtube-v31.p.rapidapi.com";
+const options = {
+  params: {
+    maxResults: "50",
+  },
+  headers: {
+    "X-RapidAPI-Key": "516a7e20bbmsh9bfaa2e29fbcca7p155931jsn982bfa2195bf",
+    "X-RapidAPI-Host": "youtube-v31.p.rapidapi.com",
+  },
+};
+
+export const fetchFromAPI = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+
+  return data;
+};
